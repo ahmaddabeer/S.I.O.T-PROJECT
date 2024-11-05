09e5afcde5f295ef4468ab557d25ad1eab492abe
Your Name <ahmaddabeer36@gmail.com>
2024-11-06 00:00:03 +0530

    Apne changes ka description likhen yahan

--- PPS_PROJECTS_PRACTICAL/NOVEMBER2024/01_Project.c
+++ PPS_PROJECTS_PRACTICAL/NOVEMBER2024/01_Project.c
@@ -1,2 +1,6 @@
-//Object: Write a program in c to input any alpha bed and check the given alphabed
-//DATE:04|11|2024
+// Object: Write a program in c to input any alpha bed and check the given alphabed
+// DATE:04|11|2024
+#include <stdio.h>
+void main()
+{
+}
