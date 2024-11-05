da3124a47388a67fac5c977299e4fc683903f6b9
Your Name <ahmaddabeer36@gmail.com>
2024-11-06 03:19:33 +0530

    while loop simple example

---
+++ PPS_PROJECTS_PRACTICAL/NOVEMBER2024/05_Project.c
@@ -0,0 +1,11 @@
+//Object: Write a program in c to print welcome in SIET 100 times lets try it .
+//DATE: 05|11|2024
+#include <stdio.h>
+void main(){
+    int i = 1;
+    while(i<=100){
+        printf("WELCOME IN S.I.E.T\n");
+        i++;
+        
+    }
+}
