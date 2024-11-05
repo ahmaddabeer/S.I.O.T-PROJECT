8ffa005000c47e5a7983a8e8878ead3ed6aed218
Your Name <ahmaddabeer36@gmail.com>
2024-11-06 02:29:36 +0530

    add a new code to check vowel and consonent but using switch

--- PPS_PROJECTS_PRACTICAL/NOVEMBER2024/02_Project.c
+++ PPS_PROJECTS_PRACTICAL/NOVEMBER2024/02_Project.c
@@ -1,18 +1,16 @@
-//Object : Write a program in to input any alphabet and check the given alphabet is vowels or consonent.claculate using if.
-//DATE: 04|11|2024
+// Object : Write a program in to input any alphabet and check the given alphabet is vowels or consonent.claculate using if.
+// DATE: 04|11|2024
 #include <stdio.h>
 void main()
 {
     char c;
-    //take user charakter
+    // take user charakter
     printf("Enter charakter to check vowels or consonant :\n");
     scanf("%c", &c);
-    //calculate using if 
-    if(c == 'A'||c == 'a'||c == 'e'||c == 'E'||c == 'i'||c == 'I'||c == 'o' || c =='O'||c == 'u'|| c== 'U')
-    printf("Yes you enter a vowel \n");
+    // calculate using if
+    if (c == 'A' || c == 'a' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U')
+        printf("Yes you enter a vowel \n");
     else
-    printf("Yes this a consonent\n");
+        printf("Yes this a consonent\n");
     printf("Thank You");
-
-
 }
--- PPS_PROJECTS_PRACTICAL/NOVEMBER2024/03_Project.c
+++ PPS_PROJECTS_PRACTICAL/NOVEMBER2024/03_Project.c
@@ -1,4 +1,29 @@
+//Object : Write a program in c to check the given charakter is vowels or consonents using switch case. 
+//DTAE: 04|11|2024
 #include <stdio.h>
-void main(){
-    printf("You are a ciso");
+void main()
+{
+    char c;
+    // take user charakter
+    printf("Enter charakter to check vowels or consonant :\n");
+    scanf("%c", &c);
+    switch(c){
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'I':
+        case 'i':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+        printf("Yes this is vowel \n");
+        break;
+        default:
+        printf("Consonant\n");
+
+    }
+    printf("Thank you");
+
 }
