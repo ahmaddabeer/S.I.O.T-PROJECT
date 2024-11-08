f2a7e79b0f7bab06e2f41bbeca620076e0a61987
Your Name <ahmaddabeer36@gmail.com>
2024-11-08 19:06:27 +0530

    odd or even number 0 to 100 tak print karana ta usko loop ki help se

--- PPS_PROJECTS_PRACTICAL/NOVEMBER2024/06_check_even_odd.c
+++ PPS_PROJECTS_PRACTICAL/NOVEMBER2024/06_check_even_odd.c
@@ -1,14 +1,23 @@
-//Object: Write a program in c to print even or odd number value take by user
-//DATE:06|11|2024
+// Object: Write a program in c to print 0 to 100 first print even an then odd numbers . usign loop
+// DATE:06|11|2024
 #include <stdio.h>
-void main(){
-    int n;
-    printf("Enter a number to check given number even or odd : \n :");
-    scanf("%d", &n);
-    //Check even or odd
-    if(n%2 == 0){
-        printf("The given number %d is even\n", n);
-
-    }else
-    printf("The given number  %d is odd",n);
+void main()
+{
+    printf("Even number 0 to 100\n");
+    for (int ev = 0; ev <= 100; ev++)
+    {
+        if (ev % 2 == 0)
+        {
+            printf("%d\t", ev);
+        }
+    }
+    printf("\n\n");
+    printf("Odd number 0 to 100\n");
+    for (int odd = 0; odd <= 100; odd++)
+    {
+        if (odd % 2 != 0)
+        {
+            printf("%d\t", odd);
+        }
+    }
 }
---
+++ PPS_PROJECTS_PRACTICAL/NOVEMBER2024/08_fibonachi.c
@@ -0,0 +1,7 @@
+//Object: Write a program in c to print fibonachi series .
+//DATE:06|11|2024
+#include <stdio.h>
+void main(){
+    for()
+    
+}
