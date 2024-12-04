8fa4381d62638abbdbc5163fe6b4bc5808adfcea
Your Name <ahmaddabeer36@gmail.com>
2024-12-05 00:45:54 +0530

    calculate factorial

---
+++ PPS_PROJECTS_PRACTICAL/DECEMBER_2024/09_calculate_fact.c
@@ -0,0 +1,12 @@
+//Object: Write a program in c to print factorial.
+#include <stdio.h>
+void main(){
+    int fact=1,i,n;
+    printf("Enter number to calculate factorial \n");
+    scanf("%d", &n);
+
+    for( i=1; i<=n; i++){
+        fact = fact*i;
+
+    }printf("%d", fact);
+}
